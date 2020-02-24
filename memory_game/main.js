b28176cf675ac8cc16d170373007d8c5a64984de
Ola <alyamani33@gmail.com>
2020-02-24 16:57:35 +0300

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -3,38 +3,56 @@
 console.log("User flipped " + cardThree);**/
 
 const cards = ["queen", "queen", "king","King"];
-const cardsInPlay = [];
+	const cardInPlay = [];
 
+	/**const cardOne = cards[0];
+	cardInPlay.push(cardOne);
+	console.log("User flipped queen");
 
-function checkForMatch(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
-}
+	const cardTow = cards[2];
+	cardInPlay.push(cardTow);
+	console.log("User flipped King");
 
-function flipCard(cardId){
-
-	let cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	console.log("User flipped " + cardOne);
-
-	let cardTow = cards[2];
-	cardsInPlay.push(cardTow);
-	console.log("User flipped " + cardTow);
-	
-	checkForMatch();
-	/**if (cardsInPlay.length === 2) {
-		alert("User flipped " + cardTow);
+	if (cardInPlay.length === 2) {
+		alert("Yes it is tow card in array");
 	}
 	else{
 		alert("Sorry");
 		}
-		if (cardsInPlay === cardOne && cardsInPlay === cardTow) {
+		if (cardInPlay === cardOne && cardInPlay === cardTow) {
 		alert("You found a match!");
 	}
 	else{
 		alert("Sorry, try again.");
 		}**/
+
+		function checkForMatch(){
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+
+  console.log("You found a match!");
+
+} else {
+
+  console.log("Sorry, try again.");
+
+}
+
+}
+
+function flipCard(cardId){
+
+	let cardOne = cards[0];
+
+	cardsInPlay.push(cardOne);
+
+	console.log("User flipped " + cardOne);
+
+	let cardTow = cards[2];
+
+	cardsInPlay.push(cardTow);
+
+	console.log("User flipped " + cardTow);
+
+	checkForMatch()
 }
